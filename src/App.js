a016d1bbf6f664ec4cb190e480516aef7d228765
Hung Q. Le <lequochung@me.com>
2017-11-23 00:59:03 +0700

    Added Google Tags Manager Script

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Router, Link} from 'react-static';
 import styled, {injectGlobal} from 'styled-components';
 import Routes from 'react-static-routes';
@@ -13,7 +13,7 @@ injectGlobal`
   }
 `;
 
-const App = styled.div`
+const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -23,25 +23,29 @@ const App = styled.div`
   background: #4c3f73;
   color: white;
   font-family: 'Titillium Web', sans-serif;
-  background: -webkit-linear-gradient(
-    to top,
-    #4c3f73,
-    #6c5b7b,
-    #355c7d
-  );
-  background: linear-gradient(
-    to top,
-    #4c3f73,
-    #6c5b7b,
-    #355c7d
-  );
+  background: -webkit-linear-gradient(to top, #4c3f73, #6c5b7b, #355c7d);
+  background: linear-gradient(to top, #4c3f73, #6c5b7b, #355c7d);
 `;
 
-export default () => (
-  <Router>
-    <App>
-      <Header/>
-      <Routes/>
-    </App>
-  </Router>
-);
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Wrapper>
+          <noscript dangerouslySetInnerHTML={createNoScript()} />
+          <Header />
+          <Routes />
+        </Wrapper>
+      </Router>
+    );
+  }
+}
+
+export default App;
+
+function createNoScript() {
+  const script = `
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JF3TD7" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+  `;
+  return {__html: script};
+}
--- static.config.js
+++ static.config.js
@@ -56,10 +56,24 @@ export default {
               rel="stylesheet"
               type="text/css"
             />
+            <script dangerouslySetInnerHTML={createGTMScript()}/>
           </Head>
-          <Body>{children}</Body>
+          <Body>
+            {children}
+          </Body>
         </Html>
       );
     }
   }
 };
+
+function createGTMScript() {
+  const gtmScript = `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5JF3TD7');
+  `;
+  return {__html: gtmScript};
+}

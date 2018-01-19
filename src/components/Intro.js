a47dff105a7dfc55d2a741eeaed033f3fd0bfab7
Hung Q. Le <lequochung@me.com>
2018-01-19 21:53:06 +0700

    Updated download Urls

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -67,21 +67,21 @@ class Intro extends Component {
     const macBtn = (
       <a
         className="btn btn-lg btn-success"
-        href="https://github.com/hql287/Manta/releases/download/v1.1.1/Manta-1.1.1.dmg">
+        href="https://github.com/hql287/Manta/releases/download/v1.1.2/Manta-1.1.2.dmg">
         Download for Mac
       </a>
     );
     const winBtn = (
       <a
         className="btn btn-lg btn-success"
-        href="https://github.com/hql287/Manta/releases/download/v1.1.1/Manta.Setup.1.1.1.exe">
+        href="https://github.com/hql287/Manta/releases/download/v1.1.2/Manta.Setup.1.1.2.exe">
         Download for Windows
       </a>
     );
     const linuxBtn = (
       <a
         className="btn btn-lg btn-success"
-        href="https://github.com/hql287/Manta/releases/download/v1.1.1/Manta-1.1.1-x86_64.AppImage">
+        href="https://github.com/hql287/Manta/releases/download/v1.1.2/Manta-1.1.2-x86_64.AppImage">
         Download for Linux
       </a>
     );
